cb8d2a4682eaaef5434c9b1a0f389da5a81a292e
winnyDiese <winnydiese@gmail.com>
2023-12-27 04:59:17 +0100

    creating server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-var express = require("express")()
+var app = require("express")()
 var http = require("http").Server(app)
-var io 
+var io = require("socket.io")(http)
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+http.listen(3000, function(){
+    console.log("Server running on 3000 ")   
+})
