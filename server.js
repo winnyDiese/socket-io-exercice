0c61d4550628bce3139cd340cd1436a143f8b670
winnyDiese <winnydiese@gmail.com>
2023-12-27 05:06:33 +0100

    Add socket.io in server

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname + "/index.html")
 })
 
+io.on("connection",(socket)=>{
+    console.log("a user is connected")
+    socket.on("disconnect",()=>{
+        console.log("A user is disconnect")   
+    })
+
+    socket.on("chat message",(msg)=>{
+        console.log("message recu "+msg)   
+    })
+})
+
 http.listen(3000, function(){
     console.log("Server running on 3000 ")   
 })
